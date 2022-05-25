6e261708d07ad6deb0c18170604f39e33d4744d1
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-05-26 00:05:41 +0500

    Install & configure CSS loader

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import _ from 'lodash';
+import './style.css';
+import likeimg from './like_img.png';
+import moviesimg from './movies_img.png';
 
 function component() {
   const element = document.createElement('div');
 
   // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
 
+    // Add the image to our existing div.
+  const myIcon = new Image();
+  myIcon.src = likeimg;
+
+
+  element.appendChild(myIcon);
   return element;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -14,5 +14,17 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
      filename: '[name].bundle.js',
        path: path.resolve(__dirname, 'dist'),
      clean: true,
-   },
+     },
+   module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+           },
+        {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
  };
